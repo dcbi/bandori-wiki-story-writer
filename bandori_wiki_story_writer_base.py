89531be626e15c3392066c2b9fa748009e191ee9
dcbi <77184273+dcbi@users.noreply.github.com>
2021-01-10 21:48:17 -0500

    Update bandori_wiki_story_writer_base.py

--- bandori_wiki_story_writer_base.py
+++ bandori_wiki_story_writer_base.py
@@ -18,17 +18,20 @@ write = ''
 
 for line in f1:
 
-	if '/' in line:
-		tag = line.split('/')
-		if tag[0] == ' ':
-			f2.write('<br />\n')
-		elif tag[0] == '':
-			if tag[1] == '': write = '{{dialog|others|[line]|' + tag[2].strip() + '}}\n'
-			else: write = '{{dialog|' + tag[1].strip().lower() + '|[line]}}\n'
-		else:
-			f2.write('{{loc|' + tag[0] + '}}\n')
+	l = line.strip()
+	if l == '':
+		f2.write('<br />\n')
 		continue
-
+	elif '/' in l:
+		tag = l.split('/')
+		if tag[0] == '':
+			if tag[1] == '':
+				writeNew = '{{dialog|others|[line]|' + tag[2] + '}}\n'
+			else:
+				writeNew = '{{dialog|' + process(tag[1].strip().lower(), args.abbrev) + '|[line]}}\n'
+		else:
+			if tag[1] == '': f2.write('{{loc|' + tag[0] + '}}\n')
+		continue
 	else:
 		f2.write( write.replace('[line]', line.strip().replace('[you]', '{{USERNAME}}-san')) )
 
